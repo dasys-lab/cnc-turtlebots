6ac801daf0e568b3a3c123d925abfce2d75ab09c
Tobias <Tobi_80@gmx.net>
2015-12-14 14:08:47 +0100

    added more debug in alvar process

--- ttb_worldmodel/src/RawSensorData.cpp
+++ ttb_worldmodel/src/RawSensorData.cpp
@@ -38,6 +38,8 @@ namespace ttb
 		tf::TransformListener listener;
 		for(auto marker : alvarData->markers) {
 			geometry_msgs::PoseStamped pose_out;
+			pose_out.header.frame_id = "base_link";
+			cout << "marker frame_id: " << marker.pose.header.frame_id << endl;
 			listener.transformPose("base_link", marker.pose, pose_out);
 			cout << "marker pos x,y,z: " << pose_out.pose.position.x << " ," << pose_out.pose.position.y << " ," << pose_out.pose.position.z << endl;
 			cout << "marker orientation w,x,y,z: " << pose_out.pose.orientation.w << " ," << pose_out.pose.orientation.x << " ," << pose_out.pose.orientation.y << " ," << pose_out.pose.orientation.z << endl;
