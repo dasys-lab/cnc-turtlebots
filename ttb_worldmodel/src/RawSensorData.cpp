2404412aa8d5163e9039fead47202f2bcb5e08c7
Tobias <Tobi_80@gmx.net>
2015-12-14 15:18:54 +0100

    tests in wm

--- ttb_worldmodel/src/RawSensorData.cpp
+++ ttb_worldmodel/src/RawSensorData.cpp
@@ -41,6 +41,7 @@ namespace ttb
 			pose_out.header.frame_id = "/base_link";
 			cout << "alvar data frame_id: " << alvarData << endl;
 			cout << "marker frame_id: " << marker << endl;
+			listener.waitForTransform("base_link",marker.header.frame_id, ros::Time(0), ros::Duration(5));
 			listener.transformPose("/base_link", marker.pose, pose_out);
 			cout << "marker pos x,y,z: " << pose_out.pose.position.x << " ," << pose_out.pose.position.y << " ," << pose_out.pose.position.z << endl;
 			cout << "marker orientation w,x,y,z: " << pose_out.pose.orientation.w << " ," << pose_out.pose.orientation.x << " ," << pose_out.pose.orientation.y << " ," << pose_out.pose.orientation.z << endl;
