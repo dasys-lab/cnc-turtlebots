036a8dfbb3efb54df1a64ea29c07edb01a1d01cf
Carpe Noctem <cn@das-lab.net>
2016-01-19 16:51:59 +0100

    gradle zeugs

--- ttb_apps/installJDKandROSAndroid.sh
+++ ttb_apps/installJDKandROSAndroid.sh
@@ -1,10 +1,11 @@
+# execute this file only ONCE or reset your .bashrc
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jdk lib32stdc++6 lib32z1
 # if you have already installed another java version or oracle jdk, you can switch to openjdk-8
 # by uncommenting the following commands
-# sudo update-alternatives --config java
-# sudo update-alternatives --config javac
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
 sudo apt-get install maven
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 sudo apt-get install android
