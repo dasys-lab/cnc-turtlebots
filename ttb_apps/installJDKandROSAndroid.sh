f02f5123ff50caf8002ba06abd84dafe13a09678
Carpe Noctem <cn@das-lab.net>
2016-01-19 19:29:23 +0100

    Changed install script for ttb_apps

--- ttb_apps/installJDKandROSAndroid.sh
+++ ttb_apps/installJDKandROSAndroid.sh
@@ -2,15 +2,23 @@
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk lib32stdc++6 lib32z1
+
+sudo wget "http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz" /usr/share/
+cd /usr/share/
+sudo tar -xf android-sdk_r24.4.1-linux.tgz
+sudo rm android-sdk_r24.4.1-linux.tgz
+sudo chown cn android-sdk-linux/ -R
+./android-sdk-linux/tools/android -update sdk --no-ui
+#TODO remember to add picture
+./android-sdk-linux/tools/android
+echo "export ANDROID_HOME=/usr/share/android-sdk-linux" > ~/.bashrc
 # if you have already installed another java version or oracle jdk, you can switch to openjdk-8
 # by uncommenting the following commands
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
 sudo apt-get install maven
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 sudo apt-get install android
-sudo apt-get install ros-indigo-rosjava ros-indigo-genjava ros-indigo-rosjava-build-tools ros-indigo-rosjava-bootstrap
+sudo apt-get install ros-indigo-rosjava ros-indigo-genjava ros-indigo-rosjava-build-tools ros-indigo-rosjava-bootstrap gradle
 genjava_message_artifacts
-sudo apt-get install gradle
 cd ~/cnws
 genjava_message_artifacts -a
