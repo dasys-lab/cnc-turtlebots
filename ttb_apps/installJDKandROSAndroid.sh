4fa6bb4a82f02bf94546335521e599cf65170a65
Carpe Noctem <cn@das-lab.net>
2016-01-22 12:45:09 +0100

    corrected install script

--- ttb_apps/installJDKandROSAndroid.sh
+++ ttb_apps/installJDKandROSAndroid.sh
@@ -3,15 +3,15 @@ sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk lib32stdc++6 lib32z1
 
-sudo wget "http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz" /usr/share/
+sudo wget -P /usr/share "http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz"
 cd /usr/share/
 sudo tar -xf android-sdk_r24.4.1-linux.tgz
 sudo rm android-sdk_r24.4.1-linux.tgz
 sudo chown cn android-sdk-linux/ -R
-./android-sdk-linux/tools/android -update sdk --no-ui
+./android-sdk-linux/tools/android update sdk --no-ui
 #TODO remember to add picture
 ./android-sdk-linux/tools/android
-echo "export ANDROID_HOME=/usr/share/android-sdk-linux" > ~/.bashrc
+echo "export ANDROID_HOME=/usr/share/android-sdk-linux" >> ~/.bashrc
 # if you have already installed another java version or oracle jdk, you can switch to openjdk-8
 # by uncommenting the following commands
 sudo update-alternatives --config java
