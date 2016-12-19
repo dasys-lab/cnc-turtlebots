289cab2f352947af4f050a2116e4a0ea9998bc89
Carpe Noctem <cn@das-lab.net>
2016-12-19 15:26:11 +0100

    added new methods to DomainBehaviour

--- ttb_expressions/autogenerated/src/DomainBehaviour.cpp
+++ ttb_expressions/autogenerated/src/DomainBehaviour.cpp
@@ -1,6 +1,7 @@
 #include "DomainBehaviour.h"
-
+#include "engine/AlicaEngine.h"
 #include "SystemConfig.h"
+
 
 namespace alica
 {
@@ -12,25 +13,30 @@ namespace alica
 		// usefull stuff for general ttb behaviour programming
 		ownID = sc->getOwnRobotID();
 		wm = ttb::TTBWorldModel::get();
+		string robotName = wm->getEngine()->getRobotName() + "/";
 
 		// ros communication for ttb behaviours
 		ros::NodeHandle n;
-		velocityTopic = (*sc)["Drive"]->get<string>("Topics.VelocityTopic", NULL);
-		soundRequesTopic = (*sc)["TTBWorldModel"]->get<string>("Sensors.SoundRequestTopic", NULL);
+		velocityTopic = robotName + (*sc)["Drive"]->get<string>("Topics.VelocityTopic", NULL);
+		soundRequesTopic = robotName + (*sc)["TTBWorldModel"]->get<string>("Sensors.SoundRequestTopic", NULL);
 
-		moveBaseActionGoalTopic = (*sc)["Drive"]->get<string>("Topics.MoveBaseActionGoalTopic", NULL);
-		moveBaseGoalTopic = (*sc)["Drive"]->get<string>("Topics.MoveBaseGoalTopic", NULL);
+		moveBaseActionGoalTopic = robotName + (*sc)["Drive"]->get<string>("Topics.MoveBaseActionGoalTopic", NULL);
+		moveBaseGoalTopic = robotName + (*sc)["Drive"]->get<string>("Topics.MoveBaseGoalTopic", NULL);
 
 
 		mobile_baseCommandVelocityPub = n.advertise<geometry_msgs::Twist>(velocityTopic, 10);
 		soundRequestPub = n.advertise<sound_play::SoundRequest>(soundRequesTopic, 10);
 
-		move_base_ActionGoalPub = n.advertise<move_base_msgs::MoveBaseActionGoal>(moveBaseActionGoalTopic, 10);
+
 		move_base_simpleGoalPub = n.advertise<geometry_msgs::PoseStamped>(moveBaseGoalTopic, 10);
+
+
+		ac = new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>("move",true);
 	}
 
 	DomainBehaviour::~DomainBehaviour()
 	{
+		delete ac;
 	}
 
 	void alica::DomainBehaviour::send(geometry_msgs::Twist& tw)
@@ -45,7 +51,17 @@ namespace alica
 		move_base_simpleGoalPub.publish(mbsg);
 	}
 
-	void alica::DomainBehaviour::send(move_base_msgs::MoveBaseActionGoal& mbag) {
-		move_base_ActionGoalPub.publish(mbag);
+	void alica::DomainBehaviour::send(move_base_msgs::MoveBaseGoal& mbag) {
+		ac->sendGoal(mbag);
 	}
+
+	actionlib::SimpleClientGoalState alica::DomainBehaviour::getMoveState() {
+		auto state = ac->getState();
+		return state;
+	}
+
+	void alica::DomainBehaviour::cancelGoal(){
+		ac->cancelAllGoals();
+	}
+
 } /* namespace alica */
