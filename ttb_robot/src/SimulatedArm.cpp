71046390e83d7ae46e760bac3f004b2b317cf896
Stefan Jakob <jakob@vs.uni-kassel.de>
2018-01-25 12:06:01 +0100

    working on arm

--- ttb_robot/src/SimulatedArm.cpp
+++ ttb_robot/src/SimulatedArm.cpp
@@ -9,22 +9,24 @@ namespace robot
 
 SimulatedArm::SimulatedArm()
 {
-	this->sc = supplementary::SystemConfig::getInstance();
-	this->carriedObjectName = "";
-	ros::NodeHandle n;
-	this->armCmdPub = n.advertise<ttb_msgs::GrabDropObject>("/ArmCmd", 5, false);
-	this->robotName = this->sc->getHostname();
-	std::string ownTopic = "/" + this->robotName + "/ArmCmd";
-	this->armCmdSub = n.subscribe(ownTopic.c_str(), 10, &SimulatedArm::onOwnArmCmd, (SimulatedArm*)this);
-	this->interactWithObject = 0;
+    this->sc = supplementary::SystemConfig::getInstance();
+    this->carriedObjectName = "";
+    this->requestedObject = "";
+    this->armRange = 1.0;
+    ros::NodeHandle n;
+    this->armCmdPub = n.advertise<ttb_msgs::GrabDropObject>("/ArmCmd", 5, false);
+    this->robotName = this->sc->getHostname();
+    std::string ownTopic = "/" + this->robotName + "/ArmCmd";
+    this->armCmdSub = n.subscribe(ownTopic.c_str(), 10, &SimulatedArm::onOwnArmCmd, (SimulatedArm *)this);
+    this->interactWithObject = ObjectInteraction::waiting;
     spinner = new ros::AsyncSpinner(4);
     spinner->start();
 }
 
 SimulatedArm::~SimulatedArm()
 {
-	spinner->stop();
-	delete spinner;
+    spinner->stop();
+    delete spinner;
 }
 
 const std::string &SimulatedArm::getCarriedObjectName() const
@@ -32,71 +34,70 @@ const std::string &SimulatedArm::getCarriedObjectName() const
     return carriedObjectName;
 }
 
+SimulatedArm::ObjectInteraction SimulatedArm::mayInteractWithObject()
+{
+    return this->interactWithObject;
+}
+
 bool SimulatedArm::grabObject(std::string objectName)
 {
-	if(objectName.empty() || !this->carriedObjectName.empty())
-	{
-		return false;
-	}
-	ttb_msgs::GrabDropObject msg;
-	msg.senderName = this->robotName;
-	msg.objectName = objectName;
-	msg.action = ttb_msgs::GrabDropObject::GRAB;
-	this->armCmdPub.publish(msg);
-	this->interactWithObject = 0;
-	return true;
+    if (objectName.empty() || !this->carriedObjectName.empty() || !this->requestedObject.empty())
+    {
+        return false;
+    }
+    ttb_msgs::GrabDropObject msg;
+    msg.senderName = this->robotName;
+    msg.objectName = objectName;
+    msg.action = ttb_msgs::GrabDropObject::GRAB;
+    this->armCmdPub.publish(msg);
+    this->interactWithObject = ObjectInteraction::waiting;
+    this->requestedObject = objectName;
+    return true;
 }
 
 bool SimulatedArm::drobObject(std::string objectName)
 {
-	if(objectName.empty() || this->carriedObjectName.compare(objectName) != 0)
-	{
-		return false;
-	}
-	ttb_msgs::GrabDropObject msg;
-	msg.senderName = this->robotName;
-	msg.objectName = objectName;
-	msg.action = ttb_msgs::GrabDropObject::DROP;
-	this->armCmdPub.publish(msg);
-	this->interactWithObject = 0;
-	return true;
-}
-
-
-int SimulatedArm::mayInteractWithObject()
-{
-	return this->interactWithObject;
+    if (objectName.empty() || this->carriedObjectName.compare(objectName) != 0)
+    {
+        return false;
+    }
+    ttb_msgs::GrabDropObject msg;
+    msg.senderName = this->robotName;
+    msg.objectName = objectName;
+    msg.action = ttb_msgs::GrabDropObject::DROP;
+    this->armCmdPub.publish(msg);
+    this->interactWithObject = ObjectInteraction::waiting;
+    this->requestedObject = objectName;
+    return true;
 }
 
 void SimulatedArm::onOwnArmCmd(ttb_msgs::GrabDropObjectPtr msg)
 {
-	if(msg->action == ttb_msgs::GrabDropObject::GRAB)
-	{
-		if(!msg->objectName.empty() && msg->senderName.compare(this->robotName) == 0)
-		{
-			this->carriedObjectName = msg->objectName;
-			this->interactWithObject = 1;
-		}
-		else
-		{
-			this->carriedObjectName = "";
-			this->interactWithObject = -1;
-		}
-	}
-	else
-	{
-		if(!msg->objectName.empty() && msg->senderName.compare(this->robotName) == 0)
-		{
-			this->carriedObjectName = "";
-			this->interactWithObject = 1;
-		}
-		else
-		{
-			this->interactWithObject = -1;
-		}
-	}
+    if (msg->objectName.empty())
+    {
+        this->requestedObject = "";
+        this->interactWithObject = ObjectInteraction::noInteractionAllowed;
+        return;
+    }
+    if (msg->action == ttb_msgs::GrabDropObject::GRAB)
+    {
+        if (msg->senderName.compare(this->robotName) == 0 && msg->objectName.compare(this->requestedObject) == 0)
+        {
+            this->carriedObjectName = msg->objectName;
+            this->requestedObject = "";
+            this->interactWithObject = ObjectInteraction::interactionAllowed;
+        }
+    }
+    else
+    {
+        if (msg->senderName.compare(this->robotName) == 0 && msg->objectName.compare(this->requestedObject) == 0)
+        {
+            this->carriedObjectName = "";
+            this->requestedObject = "";
+            this->interactWithObject = ObjectInteraction::interactionAllowed;
+        }
+    }
 }
 
 } /* namespace robot */
 } /* namespace ttb */
-
