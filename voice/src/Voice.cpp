585307334ae1ac44d57ef9560f811875a8715cc4
lab-pc5 <cn@das-lab.net>
2018-12-06 12:20:43 +0100

    make the program react to capnzero messages

--- voice/src/Voice.cpp
+++ voice/src/Voice.cpp
@@ -1,19 +1,61 @@
-#include <stdio.h>
+#include <capnzero-base-msgs/string.capnp.h>
+
+#include <zmq.h>
+
+#include <capnzero/Common.h>
+#include <capnzero/Subscriber.h>
+#include <capnp/serialize.h>
+
 #include <string>
+
+#include <signal.h>
 
 std::string cmd = "flite -t ";
 
-int talk(std::string text)
+static bool interrupted = false;
+static void s_signal_handler(int signal_value)
 {
-    std::system((cmd+text).c_str());
+    interrupted = true;
 }
 
+static void s_catch_signals(void)
+{
+    struct sigaction action;
+    action.sa_handler = s_signal_handler;
+    action.sa_flags = 0;
+    sigemptyset(&action.sa_mask);
+    sigaction(SIGINT, &action, NULL);
+    sigaction(SIGTERM, &action, NULL);
+}
 
+void talk(::capnp::FlatArrayMessageReader& reader)
+{
+    std::system((cmd+"\""+reader.getRoot<capnzero::String>().getString().cStr()+"\"").c_str());
+//    std::system((cmd+"\""+reader.getRoot<capnzero::String>().toString().flatten().cStr()+"\"").c_str());
+}
 
 int main(int argc, char **argv)
 {
-    std::string text = "\"hello World\"";
+    // register signals
+    s_catch_signals();
 
-    talk(text);
+
+    // setup capnzero
+    void* ctx = zmq_ctx_new();
+    capnzero::Subscriber* sub = new capnzero::Subscriber(ctx, argv[1]);
+    sub->connect(capnzero::CommType::IPC, "@capnzero.ipc");
+//    sub->connect(capnzero::CommType::UDP, "*:5555");
+    sub->subscribe(&talk);
+
+    // wait
+    while (!interrupted) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    // clean up
+    delete sub;
+    zmq_ctx_term(ctx);
+
+    return 0;
 }
 
